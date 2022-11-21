c65252bd3e73bcc3f6160a2236163be371844063
Abriansyah Adam <abriansyah.adam.ft21@mail.umy.ac.id>
2022-11-21 15:41:30 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise_Linked_List_D
+{
+    class Node
+    {
+
+    }
+    class CircularList
+    {
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
